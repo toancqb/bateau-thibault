8aeee27ea36c3acf48203453d0e8bd4af2b42f0b
toancqb <toantqcqb@gmail.com>
2021-10-18 16:55:37 +0200

    feature/A-Vue0 init

---
+++ Angular-Ionic/src/pages/accueil/accueil.page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-accueil-page',
+    templateUrl: './accueil.page.component.html',
+    styleUrls: ['./accueil.page.component.scss']
+})
+
+export class AccueilPageComponent {
+
+}
