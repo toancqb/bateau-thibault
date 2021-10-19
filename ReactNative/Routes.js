11b11405f6280b60ea58d7b7716d2c36dded7f3b
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2021-10-19 11:35:36 +0200

    Add routes

--- ReactNative/Routes.js
+++ ReactNative/Routes.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import ProductsAndPromotionsScreen from './screens/ProductsAndPromotionsScreen'
 import React from 'react'
 
 const Stack = createStackNavigator();
@@ -9,8 +10,19 @@ const Routes = () => (
     <NavigationContainer>
         <Stack.Navigator>
             <Stack.Screen
+                options={{ headerShown: false }}
                 name="Home"
                 component={HomeScreen}
+            />
+            <Stack.Screen
+                options={{
+                    title: 'Le bateau de Thibault', 
+                    headerStyle: {
+                        textAlign: 'center',
+                    },
+                }}
+                name="ProductsAndPromotions"
+                component={ProductsAndPromotionsScreen}
             />
         </Stack.Navigator>
     </NavigationContainer>
