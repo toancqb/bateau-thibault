d21efd4250e1885023927679f77ca0ed7f105052
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2021-10-19 16:13:12 +0200

    Add home page for vue 5

--- ReactNative/screens/ProductsAndPromotionsScreen.jsx
+++ ReactNative/screens/ProductsAndPromotionsScreen.jsx
@@ -1,26 +1,23 @@
 import { Text, View, FlatList, StyleSheet } from "react-native"
 import React from 'react'
 import { Background } from "../components/templates"
+import { Button } from "../components/atoms"
+import PoupleIcon from '../assets/images/poulpe@3x.png'
 
 export default function ProductsAndPromotions() {
     return (
         <Background>
-            <Text>Choissisez vos produits</Text>
+            <Text style={styles.text}>Choissisez vos produits</Text>
             <View style={styles.container}>
                 <FlatList
                     data={[
-                        { key: 'Devin' },
-                        { key: 'Dan' },
-                        { key: 'Dominic' },
-                        { key: 'Jackson' },
-                        { key: 'James' },
-                        { key: 'Joel' },
-                        { key: 'John' },
-                        { key: 'Jillian' },
-                        { key: 'Jimmy' },
-                        { key: 'Julie' },
+                        { key: 'Poissons' },
+                        { key: 'Coquillages' },
+                        { key: 'Crustacés' },
+                        { key: 'Promotions' },
                     ]}
-                    renderItem={({ item }) => <View style={styles.listItem}><Text style={styles.item}>{item.key}</Text></View>}
+                    renderItem={({ item }) => <View key={item.key} style={styles.listItem}>
+                        <Button tinyLogoHeight={100} tinyLogoWidth={100} minHeight={128} iconSrc={PoupleIcon} title={item.key} /></View>}
                 />
             </View>
         </Background>
@@ -33,14 +30,21 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 22
     },
+    text: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 16
+    },
     item: {
         padding: 10,
         fontSize: 18,
-        height: 44,
+        height: 120,
         color: 'white'
     },
     listItem: {
-        backgroundColor: 'rgba(0,0,0,0.3)'
+        marginTop: 16,
+        marginBottom: 16,
     }
 })
 
