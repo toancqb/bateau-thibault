d6d9f026f72032ed5ef1f7463275284117ff9f9e
zerodiverse <59115514+zerodiversex@users.noreply.github.com>
2021-10-20 09:43:43 +0200

    Refactor React (#11)

--- ReactNative/Routes.js
+++ ReactNative/Routes.js
@@ -1,6 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import BateauxScreen from './screens/BateauxScreen';
+import RestaurantsScreen from './screens/RestaurantsScreen';
 import DetailScreen from './screens/DetailScreen';
 import RecetteScrean from './screens/RecetteScreen';
 import HomardScreen from './screens/Recettes/HomardScreen';
@@ -8,11 +10,32 @@ import StJacquesScreen from './screens/Recettes/StJacquesScreen';
 import BarScreen from './screens/Recettes/BarScreen';
 import TourteauScreen from './screens/Recettes/TourteauScreen';
 import RecetteXYZScreen from './screens/Recettes/RecetteXYZ';
-import ProductsAndPromotionsScreen from './screens/ProductsAndPromotionsScreen'
+import ProductsAndPromotionsScreen from './screens/ProductsAndPromotionsScreen';
+import { StyleSheet } from 'react-native';
 import React from 'react'
+import {Image, Text, View} from "react-native-web";
+import cartIcon from './assets/images/cartLogo.png'
+import homeIcon from './assets/images/homeLogo.png'
+import {ImageHeader} from "./components/molecules";
 
 const Stack = createStackNavigator();
 
+const headerSpecialOption = {
+    headerTitle: (props) => <ImageHeader {...props}/>,
+    headerTintColor: '#fff',
+    headerTitleAlign: 'center',
+    headerStyle: {
+        textAlign: 'center',
+        flex: 1,
+        backgroundColor: 'rgb(0,0,0)',
+    },
+    headerRight: () => (
+        <View><Image style={styles.homeLogo} source={cartIcon}/></View>
+    ),
+    headerLeft: (props) => (
+        <Text {...props}><Image  style={styles.homeLogo} source={homeIcon}/></Text>
+    ),
+}
 
 const Routes = () => (
     <NavigationContainer>
@@ -26,23 +49,7 @@ const Routes = () => (
                 component={HomeScreen}
             />
             <Stack.Screen
-                options={{
-                    title: 'Hello',
-                    headerTitle: (props) => <ImageHeader {...props}/>,
-                    headerTintColor: '#fff',
-                    headerTitleAlign: 'center',
-                    headerStyle: {
-                        textAlign: 'center',
-                        flex: 1,
-                        backgroundColor: 'rgb(0,0,0)',
-                    },
-                    headerRight: () => (
-                        <View><Image style={styles.homeLogo} source={cartIcon}/></View>
-                    ),
-                    headerLeft: (props) => (
-                        <Text {...props}><Image  style={styles.homeLogo} source={homeIcon}/></Text>
-                    ),
-                }}
+                options={headerSpecialOption}
                 name="ProductsAndPromotions"
                 component={ProductsAndPromotionsScreen}
             />
@@ -71,26 +78,50 @@ const Routes = () => (
                 component={RestaurantsScreen}
             />
             <Stack.Screen
+                options={{
+                    title: '',
+                    headerTransparent: true,
+                }}
                 name="Recette"
                 component={RecetteScrean}
             />
             <Stack.Screen
+                options={{
+                    title: '',
+                    headerTransparent: true,
+                }}
                 name="Homard"
                 component={HomardScreen}
             />
             <Stack.Screen
+                options={{
+                    title: '',
+                    headerTransparent: true,
+                }}
                 name="St Jacques"
                 component={StJacquesScreen}
             />
             <Stack.Screen
+                options={{
+                    title: '',
+                    headerTransparent: true,
+                }}
                 name="Bar"
                 component={BarScreen}
             />
             <Stack.Screen
+                options={{
+                    title: '',
+                    headerTransparent: true,
+                }}
                 name="Tourteau"
                 component={TourteauScreen}
             />
              <Stack.Screen
+                 options={{
+                     title: '',
+                     headerTransparent: true,
+                 }}
                 name="RecetteXYZ"
                 component={RecetteXYZScreen}
             />
